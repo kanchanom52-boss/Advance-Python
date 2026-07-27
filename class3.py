45fab889ccb21f5408514ce05ac02e4e31778148
kanchanom52-boss <kanchanom52@gmail.com>
2026-07-27 12:07:15 +0530

    Add files via upload

--- class3.py
+++ class3.py
@@ -1,32 +1,32 @@
-def decorate_report(fun):
-    def wrapper(*args):
-        print("********************")
-        fun(*args)
-        print("********************")
+def decorator(function):
+    def wrapper(*args, **kwargs):
+        print("*" * 20)
+        function(*args, **kwargs)
+        print("*" * 20)
     return wrapper
 
 class Report:
-    template = "Default Template"
-
+    template = "New Report"
+    
     def __init__(self, title, content):
         self.title = title
         self.content = content
-
+        
     @classmethod
     def change_template(cls, new_template):
         cls.template = new_template
-
+        
     def __str__(self):
         return (
-            "Template: " + self.template + "\n" +
-            "Title: " + self.title + "\n" +
-            "Content: " + self.content
+            f"Template: {self.template}\n"
+            f"Title: {self.title}\n"
+            f"Content: {self.content}"
         )
-
-    @decorate_report
+        
+    @decorator
     def show_report(self):
         print(self)
 
 Report.change_template("Student Report")
-r1 = Report("Python Project", "Dynamic Report Generator using OOP")
+r1 = Report("The 48 laws of Power", "self help")
 r1.show_report()
